ddef71ad8c60b7462e61dceb5d740051644363ba
Yevhenii Sitolenko <110056243+Yevhenii770@users.noreply.github.com>
2025-09-18 18:53:22 -0700

    sceleton dashboard

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     cacheComponents: true,
+    dynamicIO: true,
   },
   typescript: {
     ignoreBuildErrors: true,
