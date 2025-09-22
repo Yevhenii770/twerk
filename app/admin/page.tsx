b58c3a9928bfb0aee27db6a27021a733705e31f9
Yevhenii Sitolenko <110056243+Yevhenii770@users.noreply.github.com>
2025-09-21 19:04:45 -0700

    adm

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,25 +1,7 @@
-// import { getCurrentUser, getAllUsers } from "@/lib/dal";
-// import { redirect } from "next/navigation";
-
-// export default async function AdminPage() {
-//   const user = await getCurrentUser();
-
-//   //   if (!user || user.role !== "admin") {
-//   //     redirect("/signin");
-//   //   }
-
-//   const users = await getAllUsers();
-
-//   return (
-//     <div>
-//       <h1>Admin Dashboard</h1>
-//       <ul>
-//         {users.map((u) => (
-//           <li key={u.id}>
-//             {u.email} — {new Date(u.createdAt).toLocaleDateString()}
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
+export default async function AdminPage() {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+    </div>
+  );
+}
